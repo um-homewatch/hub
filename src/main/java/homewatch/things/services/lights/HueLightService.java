d55077a3b508a0738c7ce6f8f2f972e66a48b337
José Sousa <josesousa9000@gmail.com>
2017-08-02 16:38:44 +0100

    Fix hue light id discovery param

--- src/main/java/homewatch/things/services/lights/HueLightService.java
+++ src/main/java/homewatch/things/services/lights/HueLightService.java
@@ -1,5 +1,6 @@
 package homewatch.things.services.lights;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import homewatch.constants.JsonUtils;
 import homewatch.exceptions.NetworkException;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public class HueLightService extends HttpThingService<Light> {
+  @JsonProperty("light_id")
   private int lightID;
 
   HueLightService() {
